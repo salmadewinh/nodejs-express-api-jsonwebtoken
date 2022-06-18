2f92bf08eec63680be7047be4149be3fc24a2f68
it'sme <94110881+salmadewinh@users.noreply.github.com>
2022-06-18 23:50:19 +0700

    Update database.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
     host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    user                : "sql6500666",
+    password            : "whaHyBkKva",
+    database            : "sql6500666"
 };
