d5b6fc9bcdc8d96ff3cc8f3a06e6acc9757f029f
it'sme <94110881+salmadewinh@users.noreply.github.com>
2022-06-19 00:00:12 +0700

    Update route-karyawan.js

--- src/routes/route-karyawan.js
+++ src/routes/route-karyawan.js
@@ -26,18 +26,18 @@ function AuthenticateAccessToken(req,res,next){
 }
 
 // GET localhost:8080/karyawan => Ambil data semua karyawan
-router.get('/karyawan', AuthenticateAccessToken, karyawan.getDataKaryawan);
+router.get('/karyawan', karyawan.getDataKaryawan);
 
 // GET localhost:8080/karyawan/2 => Ambil data semua karyawan berdasarkan id = 2
 router.get('/karyawan/:id', karyawan.getDataKaryawanByID);
 
 // POST localhost:8080/karyawan/add => Tambah data karyawan ke database
-router.post('/karyawan/add', karyawan.addDataKaryawan);
+router.post('/karyawan/add', AuthenticateAccessToken, karyawan.addDataKaryawan);
 
 // POST localhost:8080/karyawan/2 => Edit data karyawan
-router.post('/karyawan/edit', karyawan.editDataKaryawan);
+router.post('/karyawan/edit', AuthenticateAccessToken, karyawan.editDataKaryawan);
 
 // POST localhost:8080/karyawan/delete => Delete data karyawan
-router.post('/karyawan/delete/', karyawan.deleteDataKaryawan);
+router.post('/karyawan/delete/', AuthenticateAccessToken, karyawan.deleteDataKaryawan);
 
 module.exports = router;
